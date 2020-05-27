4b7391195d50209d136bf1ac2d4775fadc8691c1
Fahim Ali Zain <faztp12@gmail.com>
2020-05-27 13:08:23 +0200

    chore: Added docs for TempFiles

--- renovation_core/renovation_core/doctype/temporary_file/temporary_file.py
+++ renovation_core/renovation_core/doctype/temporary_file/temporary_file.py
@@ -14,25 +14,34 @@ class TemporaryFile(Document):
     self.validate_target_fieldname()
 
   def validate_file_url(self):
+    """
+    Validate if a File document actually exists with the specified URL
+    """
     if not frappe.db.get_value("File", {"file_url": self.file}):
       frappe.throw("No file exists with url {}".format(self.file))
 
   def validate_target_docname(self):
-    if not self.target_dn:
+    """
+    Validate if a document with specified name exists in the DocType
+    """
+    if not self.target_docname:
       return
 
-    if not frappe.db.exists(self.target_dt, self.target_dn):
+    if not frappe.db.exists(self.target_doctype, self.target_docname):
       frappe.throw("Document {}: {} doesnt exist".format(
-          self.target_dt, self.target_dn))
+          self.target_doctype, self.target_docname))
 
   def validate_target_fieldname(self):
-    if not self.target_df:
+    """
+    Validate if the fieldname specified is actually an Attach/Attach Image field in the target_doctype
+    """
+    if not self.target_fieldname:
       frappe.throw("Target DF is mandatory")
-    m = frappe.get_meta(self.target_dt)
-    df = m.get_field(self.target_df)
+    m = frappe.get_meta(self.target_doctype)
+    df = m.get_field(self.target_fieldname)
     if not df:
       frappe.throw("Fieldname {} doesnt exist in doctype {}".format(
-          self.target_df, self.target_dt))
+          self.target_fieldname, self.target_doctype))
 
     if df.fieldtype not in ("Attach", "Attach Image"):
       frappe.throw("{} is not an attachment field".format(df.fieldname))	
--- renovation_core/utils/temporary_files.py
+++ renovation_core/utils/temporary_files.py
@@ -3,20 +3,27 @@ import frappe
 """
 Temporary File {
   file: Attach
-  target_dt: Link/DocType
-  targent_dn: DynamicLink/target_dt
-  target_df: Data
+  target_doctype: Link/DocType
+  target_docname: DynamicLink/target_doctype
+  target_fieldname: Data
 }
 """
 
 
 def flush_files():
+  """
+  Go through each Temporary File and check if it has gone past the expiry time or has been attached to some other document
+  If it is expired, delete both the actual File and the temporary File
+  If the file has been attached to some other document, delete this Temporary File only.
+
+  This function is called attached to the scheduler to be invoked every hour
+  """
   temp_files = frappe.get_all("Temporary File", fields=["*"])
   if not len(temp_files):
     return
 
   for t in temp_files:
-    if file_exists(dt=t.target_dt, dn=t.targent_dn, df=t.target_df, file=t.file):
+    if file_exists(dt=t.target_doctype, dn=t.target_docname, df=t.target_fieldname, file=t.file):
       frappe.delete_doc("Temporary File", t.name)
     elif temp_file_expired(t):
       safe_delete_file(t.file)
@@ -24,6 +31,14 @@ def flush_files():
 
 
 def file_exists(dt, dn, df, file):
+  """
+  Checks if the file has been linked to the specified document or not.
+
+  :param dt: The doctype to check in
+  :param dn: The docname to check in
+  :param df: The fieldname to check in
+  :param file: the file_url to check for
+  """
   r = frappe.db.sql("""
     SELECT
       d.name
@@ -38,6 +53,15 @@ def file_exists(dt, dn, df, file):
 
 
 def temp_file_expired(temp_file):
+  """
+  Checks if the TempFile has lived past its expiry
+
+  :param temp_file {
+    creation: datetime
+    expires_in_hours: int
+    name: string
+  }
+  """
   from frappe.utils import add_to_date, cint, now_datetime
 
   if add_to_date(temp_file.creation, hours=cint(temp_file.expires_in_hours) or 5) >= now_datetime():
@@ -47,8 +71,12 @@ def temp_file_expired(temp_file):
 
 
 def safe_delete_file(file_url):
+  """
+  Delete the File iff it has'nt been attached to any other docs
+  :param file_url: the file_url to check against
+  """
   file_id = frappe.db.get_value("File", {"file_url": file_url})
   if not file_id:
     return
-  
+
   frappe.delete_doc("File", file_id)
