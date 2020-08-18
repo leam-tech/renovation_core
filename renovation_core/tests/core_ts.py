1cf639b8022f3b35b6a7f1828fa6284432594df5
Fahim Ali Zain <faztp12@gmail.com>
2020-08-18 23:46:47 +0530

    tests: TEST DOCTYPE

--- renovation_core/tests/core_ts.py
+++ renovation_core/tests/core_ts.py
@@ -1,10 +1,11 @@
 import frappe
+from frappe.utils import cint
 
 
 def init_site():
   print("Initializing site for core-ts tests")
   # Lets check if setup_complete, else lets do it first
-  if not frappe.db.get_value('System Settings', 'System Settings', 'setup_complete'):
+  if not cint(frappe.db.get_value('System Settings', 'System Settings', 'setup_complete')):
     from frappe.desk.page.setup_wizard.setup_wizard import setup_complete
     setup_complete({
         "language": "English", "country": "United Arab Emirates", "timezone": "Asia/Dubai", "currency": "AED", "full_name": "Example Admin",
@@ -14,6 +15,7 @@ def init_site():
   create_users()
   create_dashboards()
   create_reports()
+  create_test_doctype()
   print("Done!")
   print("NB: Please create a secondary site for ENV: CORE_TS_HOST_URL_SECONDARY")
 
@@ -84,3 +86,48 @@ def create_users():
         quick_login_pin="1234",
         send_welcome_email=0
     )).insert()
+
+
+def create_test_doctype():
+  if frappe.db.exists("DocType", "TEST DOCTYPE"):
+    return
+
+  # Child Doctype
+  frappe.get_doc(frappe._dict(
+      doctype="DocType",
+      __newname="TEST DOCTYPE ITEM",
+      module="Renovation Core",
+      custom=1,
+      editable_grid=1,
+      istable=1,
+      fields=[
+          frappe._dict(label="Title", fieldtype="Data", fieldname="title", in_list_view=1)
+      ]
+  )).insert()
+
+  doc = frappe.get_doc(frappe._dict(
+      doctype="DocType",
+      __newname="TEST DOCTYPE",
+      module="Renovation Core",
+      custom=1,
+      allow_rename=1,
+      allow_import=1,
+      track_changes=1,
+      autoname="field:title",
+      is_submittable=1,
+      fields=[
+          frappe._dict(fieldtype="Data", fieldname="title",
+                       label="Document Title"),
+          frappe._dict(fieldtype="Table", fieldname="items",
+                       options="TEST DOCTYPE ITEM", label="Items")
+      ],
+      permissions=[
+          frappe._dict(
+              role="System Manager", share=1, submit=1, write=1,
+              amend=1, cancel=1, create=1, delete=1, docstatus=0,
+              email=1, export=1, permlevel=0, print=1, read=1, report=1
+          )
+      ]
+  ))
+  doc.permissions[0].set("import", 1)
+  doc.insert()
