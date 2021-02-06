b1190338dc05d4b2379bd1c985a3097b5d809385
Elton Lobo <elton1231@gmail.com>
2021-02-06 10:21:58 +0400

    feat: huawei user token

---
+++ renovation_core/renovation_core/doctype/huawei_user_token/huawei_user_token.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, LEAM Technology System and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Huawei User Token', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ renovation_core/renovation_core/doctype/huawei_user_token/huawei_user_token.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, LEAM Technology System and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+# import frappe
+from frappe.model.document import Document
+
+class HuaweiUserToken(Document):
+	pass
