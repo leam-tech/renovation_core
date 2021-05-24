77c593dc54bd994271c4b1d4068311803a93b657
Fahim Ali Zain <faztp12@gmail.com>
2021-05-24 09:44:10 +0530

    fix: SMS Settings Providers

--- renovation_core/utils/sms_setting.py
+++ renovation_core/utils/sms_setting.py
@@ -48,7 +48,6 @@ def send_sms(receiver_list, msg, sender_name='', success_msg=True, provider=None
   elif isinstance(provider, string_types):
     provider = [{"provider": provider, "country": x.country, "code": x.code.lower()} for x in (
         frappe.get_cached_doc("SMS Provider", provider).get("countries") or [])]
-    provider.append({"provider": provider, "country": "all", "code": "all"})
   if provider:
     return send_via_gateway(arg, providers=provider)
   else:
@@ -127,9 +126,9 @@ def _get_provider_validate_data(provider):
   provider_wise_time = frappe._dict()
   for x in provider or []:
     key = x.get("code") or "all"
-    country_wise_provider.setdefault(key.lower(), []).append(x.provider)
+    country_wise_provider.setdefault(key.lower(), []).append(x.get("provider"))
     provider_wise_time.setdefault(
-        x.get("provider"), frappe.get_cached_doc("SMS Provider", x.provider).get("timing") or [])
+        x.get("provider"), frappe.get_cached_doc("SMS Provider", x.get("provider")).get("timing") or [])
   return country_wise_provider, provider_wise_time
 
 
@@ -145,7 +144,7 @@ def send_request(gateway_url, params, headers=None, use_post=False, request_as_j
       response = requests.post(gateway_url, headers=headers, data=params)
   else:
     response = requests.get(gateway_url, headers=headers, params=params)
-  response.raise_for_status()
+
   return response.status_code
 
 
