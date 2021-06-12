7a3df9e2b7c9445a9170ab41ad38d0411b111889
Assem Marwan <assem905@gmail.com>
2021-06-12 09:14:34 +0400

    chore: Bump to 1.6.2

--- renovation_core/__init__.py
+++ renovation_core/__init__.py
@@ -12,7 +12,7 @@ from .utils.notification import send_notification
 from .utils.sms_setting import validate_receiver_nos
 from .utils.sync import _get_doc_files, process
 
-__version__ = '1.6.1'
+__version__ = '1.6.2'
 
 Meta.process = process
 frappe.model.sync.get_doc_files = _get_doc_files
