d02c95e6eb82f4d93443c51204464f7644db58f1
Fahim Ali Zain <fahimalizain@gmail.com>
2022-05-09 14:08:31 +0000

    [PMS-T-212] feat: async renovation.log_error fix: moved title default value for renovation.log_error
    fixup! feat: async renovation.log_error
    feat: async renovation.log_error
    
    
    Co-authored-by: Fahim Ali Zain <fahimalizain@gmail.com>
    
    Merge-request: PMS-MR-93
    Merged-by: Assem Marwan <assem@leam.ae>

--- renovation/__init__.py
+++ renovation/__init__.py
@@ -10,7 +10,7 @@ from .model import FrappeModel as RenovationModel  # noqa
 # Useful utilities
 from frappe import (local, _, parse_json, as_json, _dict, get_module, get_roles, get_meta,  # noqa
      get_hooks, get_traceback, scrub, set_user, has_permission, whitelist, is_whitelisted,  # noqa
-     get_doc as frappe_get_doc, cache, generate_hash, safe_decode, safe_encode)  # noqa
+     get_doc as frappe_get_doc, cache, generate_hash, safe_decode, safe_encode, log_error as _log_error)  # noqa
 from frappe.utils import cint, flt, now_datetime, nowdate, nowtime, add_to_date, DATE_FORMAT, DATETIME_FORMAT, TIME_FORMAT  # noqa
 from frappe.core.doctype.version.version import get_diff  # noqa
 
@@ -30,5 +30,11 @@ def get_attr(method_string):
 async def get_doc(*args, **kwargs):
     return await asyncify(frappe_get_doc)(*args, **kwargs)
 
+
+async def log_error(title: str, message: str = None):
+    """Log error to Error Log"""
+    if not title:
+        title = _("Error")
+    return await asyncify(_log_error)(title=title, message=message)
 
 user = LocalProxy(lambda: local.session.user)
