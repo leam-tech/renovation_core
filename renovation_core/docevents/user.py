9cae1041ba1f361d6c1cc2250de5595edd2462e7
Abadulrehman <Abadulrehman@gmail.com>
2022-10-20 04:04:35 +0000

    fix: Ignore permissions for OAuth deletion on User deletion
    Co-authored-by: Abadulrehman <Abadulrehman@gmail.com>
    
    Merge-request: PMS-MR-458
    Merged-by: Fahim Ali Zain <fahim.zain@leam.ae>

--- renovation_core/docevents/user.py
+++ renovation_core/docevents/user.py
@@ -3,4 +3,4 @@ import frappe
 
 def on_trash(doc, method=None):
     for token in frappe.get_all("OAuth Bearer Token", {"user": doc.name}):
-        frappe.delete_doc("OAuth Bearer Token", token.name)
+        frappe.delete_doc("OAuth Bearer Token", token.name, ignore_permissions=True)
