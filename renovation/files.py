bdf109d831f44cc9397d15296561d91da17883b4
Abadulrehman <Abadulrehman@gmail.com>
2022-10-29 07:39:18 +0400

    fix: get content in async

--- renovation/files.py
+++ renovation/files.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, HTTPException
+from asyncer import asyncify
 from starlette.responses import Response
 
 import frappe
@@ -8,25 +9,26 @@ router = APIRouter()
 
 
 @router.get("/files/{filename}")
-def get_public_file(filename: str):
-    return get_file_as_response(f"/files/{filename}")
+async def get_public_file(filename: str):
+    return await get_file_as_response(f"/files/{filename}")
 
 
 @router.get("/private/files/{filename}")
-def get_private_file(filename: str):
+async def get_private_file(filename: str):
 
     if frappe.session.user == "Guest":
         raise HTTPException(status_code=401, detail="Not allowed")
 
-    return get_file_as_response(f"/private/files/{filename}")
+    return await get_file_as_response(f"/private/files/{filename}")
 
 
-def get_file_as_response(file_url: str):
+async def get_file_as_response(file_url: str):
 
     name_of_file = frappe.get_value("File", {"file_url": file_url}, "name")
     file = frappe.get_doc("File", name_of_file)
 
     import mimetypes
     filetype = mimetypes.guess_type(file.file_url)[0]
+    file_content = await asyncify(file.get_content)()
 
-    return Response(content=file.get_content(), media_type=filetype)
+    return Response(content=file_content, media_type=filetype)
