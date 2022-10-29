ebe5e46b31306b8af0392f0254f57f6830608e2f
Abadulrehman <Abadulrehman@gmail.com>
2022-10-29 06:12:58 +0400

    fix: guess file mimetype

--- renovation/files.py
+++ renovation/files.py
@@ -9,11 +9,7 @@ router = APIRouter()
 
 @router.get("/files/{filename}")
 def get_public_file(filename: str):
-
-    file_url = f"/files/{filename}"
-    name_of_file = frappe.get_value("File", {"file_url": file_url}, "name")
-    file = frappe.get_doc("File", name_of_file)
-    return Response(content=file.get_content(), media_type="image")
+    return get_file_as_response(f"/files/{filename}")
 
 
 @router.get("/private/files/{filename}")
@@ -22,7 +18,15 @@ def get_private_file(filename: str):
     if frappe.session.user == "Guest":
         raise HTTPException(status_code=401, detail="Not allowed")
 
-    file_url = f"/private/files/{filename}"
+    return get_file_as_response(f"/private/files/{filename}")
+
+
+def get_file_as_response(file_url: str):
+
     name_of_file = frappe.get_value("File", {"file_url": file_url}, "name")
     file = frappe.get_doc("File", name_of_file)
-    return Response(content=file.get_content(), media_type="image")
+
+    import mimetypes
+    filetype = mimetypes.guess_type(file.file_url)[0]
+
+    return Response(content=file.get_content(), media_type=filetype)
